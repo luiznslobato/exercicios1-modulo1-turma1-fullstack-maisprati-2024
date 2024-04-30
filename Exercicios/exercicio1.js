679061eb20bfaa5e3399e28cef03b68a95d49f71
luiznslobato <contato.luiznonato@gmail.com>
2024-04-30 20:56:29 -0300

    Exercício 1 resolvido

---
+++ Exercicios/exercicio1.js
@@ -0,0 +1,7 @@
+//1. Escreva um algoritmo para ler uma temperatura em graus Celsius,
+//calcular e escrever o valor correspondente em graus Fahrenheit.
+
+let temperaturaCelsius = 150
+let temperaturaFahreint = (temperaturaCelsius * 1.8) + 32
+
+console.log(temperaturaFahreint)
