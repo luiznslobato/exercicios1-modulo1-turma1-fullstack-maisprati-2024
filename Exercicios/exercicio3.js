6cdef50046e724e654d40a2da22ed6d21f4e702b
luiznslobato <contato.luiznonato@gmail.com>
2024-04-30 21:07:44 -0300

    Exercício 3 resolvido

---
+++ Exercicios/exercicio3.js
@@ -0,0 +1,14 @@
+//3. Faça um algoritmo que leia quatro números inteiros e realize as seguintes operações:
+//Some 25 ao primeiro inteiro;
+//Triplique o valor do segundo inteiro;
+//Modifique o valor do terceiro inteiro para 12% do valor original;
+//Armazene no quarto inteiro a soma dos valores originais (os que o usuário digitou) dos primeiros três inteiros.
+
+let numeroUm = 5
+let NumeroDois = 15
+let numeroTres = 20
+let numeroQuatro = numeroUm + NumeroDois + numeroTres
+
+numeroUm + 25
+NumeroDois * 3
+numeroTres * (12 / 100)
