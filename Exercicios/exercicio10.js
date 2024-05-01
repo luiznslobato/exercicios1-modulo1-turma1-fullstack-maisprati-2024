a8417020db42c26dbb41a3e8156a4707a85ae536
luiznslobato <157427972+luiznslobato@users.noreply.github.com>
2024-05-01 12:27:17 -0300

    Resolvido

--- Exercicios/exercicio10.js
+++ Exercicios/exercicio10.js
@@ -1,6 +1,8 @@
 //10. Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
 
-let numero = 10
+let prompt = require('prompt-sync')()
+
+let numero = parseInt(prompt("Digite o número: "))
 
 for (let i = 0; i < 10; i++) {
     console.log(numero)
