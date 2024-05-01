77153d960451ff8c9d6e4b473d3f884d2188b778
luiznslobato <157427972+luiznslobato@users.noreply.github.com>
2024-05-01 12:21:53 -0300

    Resolvido

--- Exercicios/exercicio4.js
+++ Exercicios/exercicio4.js
@@ -2,11 +2,14 @@
 //calcular e escrever a média semestral e a seguinte mensagem: ‘PARABÉNS! Você foi
 //aprovado’, somente se o aluno foi aprovado (considere 6.0 a nota mínima para aprovação).
 
-let notaAvaliacaoUm = 5
-let notaAvaliacaoDois = 5
-let mediaSemetral = (notaAvaliacaoUm + notaAvaliacaoDois) / 2
+let prompt = require('prompt-sync')()
 
-console.log(mediaSemetral)
-if (mediaSemetral >= 6) {
+let nota1 = Number(prompt("Defina a nota da primera avaliação: "))
+let nota2 = Number(prompt("Defina a nota da segunda avaliação: "))
+let media = (nota1 + nota2) / 2
+
+console.log(`Média semestral: ${media}`)
+
+if (media >= 6) {
     console.log("PARABÉNS! Você foi aprovado")
 }
