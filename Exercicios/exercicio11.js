090f164c20f4b95a7c21e3ed8d9664ce0a6b1b0e
luiznslobato <157427972+luiznslobato@users.noreply.github.com>
2024-05-01 12:27:46 -0300

    Resolvido

--- Exercicios/exercicio11.js
+++ Exercicios/exercicio11.js
@@ -2,17 +2,19 @@
 //cada valor fornecido escrever uma mensagem que indica se cada valor fornecido é PAR
 //ou ÍMPAR. O algoritmo será encerrado imediatamente após a leitura de um valor NULO ou NEGATIVO.
 
-let valoresInteiros = [5, 2, 25, 20, -10, 40]
+let prompt = require('prompt-sync')()
 
-for (let numero of valoresInteiros) {
+let valor = parseInt(prompt("Defina o valor: "))
 
-    if (numero == null || numero < 0) {
+while (valor !== 0) {
+
+    if (valor < 0) {
         break
-    }
-    if (numero % 2 === 0) {
-        console.log(`${numero} é PAR`)
-    }
-    else if (numero % 2 !== 0) {
-        console.log(`${numero} é ÍMPAR`)
+
+    } else if (valor % 2 === 0) {
+        console.log(`${valor} é PAR`)
+
+    } else {
+        console.log(`${valor} é ÍMPAR`)
     }
 }
