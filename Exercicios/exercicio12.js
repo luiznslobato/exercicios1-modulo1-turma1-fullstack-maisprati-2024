887faeb604cadc6296c2e8ffc6b62dbbd42bfec7
luiznslobato <157427972+luiznslobato@users.noreply.github.com>
2024-05-01 12:28:12 -0300

    Resolvido

--- Exercicios/exercicio12.js
+++ Exercicios/exercicio12.js
@@ -1,8 +1,12 @@
 //12. Escreva um algoritmo que gere os números de 1000 a 1999
 //e escreva aqueles que, divididos por 11, dão resto igual a 5.
 
-for (let i = 1000; i <= 1999; i++) {
-    if (i % 11 === 5) {
-        console.log(i)
+let contador = 1000
+
+while (contador <= 1999) {
+
+    if (contador % 11 === 5) {
+        console.log(contador)
     }
+    contador++
 }
