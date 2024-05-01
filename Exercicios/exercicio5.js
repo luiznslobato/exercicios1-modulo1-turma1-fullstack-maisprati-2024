8fab6d076fe1ab861186ba31f864860d74d33993
luiznslobato <157427972+luiznslobato@users.noreply.github.com>
2024-05-01 12:22:23 -0300

    Resolvido

--- Exercicios/exercicio5.js
+++ Exercicios/exercicio5.js
@@ -1,14 +1,17 @@
 //5. Acrescente ao exercício acima a mensagem ‘Você foi REPROVADO! Estude mais’ 
 //caso a média calculada seja menor que 6,0
 
-let notaAvaliacaoUm = 5
-let notaAvaliacaoDois = 5
-let mediaSemetral = (notaAvaliacaoUm + notaAvaliacaoDois) / 2
+let prompt = require('prompt-sync')()
 
-console.log(mediaSemetral)
-if (mediaSemetral >= 6) {
+let nota1 = Number(prompt("Defina a nota da primera avaliação: "))
+let nota2 = Number(prompt("Defina a nota da segunda avaliação: "))
+let media = (nota1 + nota2) / 2
+
+console.log(`Média semestral: ${media}`)
+
+if (media >= 6) {
     console.log("PARABÉNS! Você foi aprovado")
-}
-else {
-    console.log("REPROVADO! Estude mais")
+
+} else {
+    console.log("Você foi REPROVADO! Estude mais")
 }
