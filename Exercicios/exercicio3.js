fd85e6b6a1b630a358ce66d19e66ad05ad774fd2
luiznslobato <157427972+luiznslobato@users.noreply.github.com>
2024-05-01 12:21:22 -0300

    Resolvido

--- Exercicios/exercicio3.js
+++ Exercicios/exercicio3.js
@@ -4,11 +4,19 @@
 //Modifique o valor do terceiro inteiro para 12% do valor original;
 //Armazene no quarto inteiro a soma dos valores originais (os que o usuário digitou) dos primeiros três inteiros.
 
-let numeroUm = 5
-let NumeroDois = 15
-let numeroTres = 20
-let numeroQuatro = numeroUm + NumeroDois + numeroTres
+let prompt = require('prompt-sync')()
 
-numeroUm + 25
-NumeroDois * 3
-numeroTres * (12 / 100)
+let numero1 = parseInt(prompt("Defina o primeiro número: "))
+let numero2 = parseInt(prompt("Defina o segundo número: "))
+let numero3 = parseInt(prompt("Defina o terceiro número: "))
+let numero4 = parseInt(prompt("Defina o quarto número: "))
+
+let novoNumero1 = numero1 + 25
+let novoNumero2 = numero2 * 3
+let novoNumero3 = numero3 * (12 / 100)
+let novoNumero4 = numero1 + numero2 + numero3
+
+console.log(`O primeiro passa a valer: ${novoNumero1}`)
+console.log(`O segundo passa a valer: ${novoNumero2}`)
+console.log(`O terceiro passa a valer: ${novoNumero3}`)
+console.log(`O quarto passa a valer: ${novoNumero4}`)
