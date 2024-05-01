39d2ba70ad680048b705d8443fcf5e831d0825cf
luiznslobato <157427972+luiznslobato@users.noreply.github.com>
2024-05-01 12:20:28 -0300

    Resolvido

--- Exercicios/exercicio2.js
+++ Exercicios/exercicio2.js
@@ -2,15 +2,17 @@
 //votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa
 //em relação ao total de eleitores.
 
-let totais = 200
-let brancos = 10
-let nulos = 20
-let validos = 160
+let prompt = require('prompt-sync')()
 
-let percentualBrancos = (brancos / totais) * 100
-let percentualNulos = (nulos / totais) * 100
-let percentualValidos = (validos / totais) * 100
+let eleitores = parseInt(prompt("Defina o número de eleitores: "))
+let votosBrancos = parseInt(prompt("Defina o número de votos brancos: "))
+let votosNulos = parseInt(prompt("Defina o número de votos nulos: "))
+let votosValidos = parseInt(prompt("Defina o número de votos válidos: "))
 
-console.log(percentualBrancos)
-console.log(percentualNulos)
-console.log(percentualValidos)
+let percentualBrancos = (votosBrancos / eleitores) * 100
+let percentualNulos = (votosNulos / eleitores) * 100;
+let percentualValidos = (votosValidos / eleitores) * 100
+
+console.log(`Percentual de votos brancos: ${percentualBrancos}%`)
+console.log(`Percentual de votos nulos: ${percentualNulos}%`)
+console.log(`Percentual de votos válidos: ${percentualValidos}%`)
